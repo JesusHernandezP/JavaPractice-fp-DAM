25b0b6fba7da4f6a895656794317379ebc7379a0
Jesus Hernandez <jahp88x@gmail.com>
2024-11-10 13:01:50 +0100

    Ejercicios de nivelacion para avanzar con el aprendizaje

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,50 @@
-public class Main {
-  public static void main(String[] args) {
-    // Crear un objeto de la clase Coche
-    Coche miCoche = new Coche();
+import java.util.Scanner;
 
-    // Asignar valores a los atributos usando los métodos set
-    miCoche.setMarca("Toyota");
-    miCoche.setModelo("Corolla");
-    miCoche.setYears(2020);
-    miCoche.setVelocidad(120);
+//public class Main {
+//
+//  public static void main(String[] args){
+//    Ejercicio 1: Verificación de Números Pares e Impares
+//    Escribe un programa en Java que pida al usuario ingresar un número y determine si es par o impar. Usa estructuras de control para verificar la condición.
+//
+//    int num1 = 0;
+//
+//    Scanner scan = new Scanner(System.in);
+//    System.out.println("ingrese un numero");
+//    num1 = scan.nextInt();
+//
+//    String result = (num1 %2 == 0 ) ? "El numero es par" : "El numero es impar" ;
+//    System.out.println(result);
 
-    // Obtener y mostrar los valores usando los métodos get
-    System.out.println("Marca: " + miCoche.getMarca());
-    System.out.println("Modelo: " + miCoche.getModelo());
-    System.out.println("Año: " + miCoche.getYears());
-    System.out.println("Velocidad: " + miCoche.getVelocidad() + " km/h");
-  }
-}
+//    Ejercicio 2: Imprimir los Elementos de un Array
+//    Crea un programa que declare un array de números enteros con valores fijos y, usando un bucle, imprima cada elemento del array en la consola.
+//
+//    int [] numerosEnteros = { 26, 90, 13, 14, 65, 87, 65};
+//    for (int i : numerosEnteros  ){
+//      System.out.println(i);
+//    }
+
+//    Ejercicio 3: Calcular la Suma de Números
+//    Escribe un programa en Java que pida al usuario ingresar tres números y luego use un método para calcular la suma de esos tres números. Este método deberá ser independiente y recibir los números como parámetros.
+//
+//    Scanner scan = new Scanner(System.in);
+//
+//
+//    int num1 = 0;
+//    int num2 = 0;
+//    int num3 = 0;
+//
+//    System.out.println("ingrese 1 numero");
+//    num1 = scan.nextInt();
+//    System.out.println("ingrese 1 numero");
+//    num2 = scan.nextInt();
+//    System.out.println("ingrese 1 numero");
+//    num3 = scan.nextInt();
+//
+//    int result = num1 + num2 + num3;
+//    System.out.println("El Resultado es: " + result);
+
+
+
+
+//  }
+//}
