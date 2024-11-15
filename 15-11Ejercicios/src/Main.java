9f5c0cfd81dc5be6725b1427499341dc0c1a421a
Jesus Hernandez <jahp88x@gmail.com>
2024-11-15 14:45:18 +0100

    Practicando nuevos elementos como chartAt, toLowerCase, indexOf, Character.isLetter

--- 15-11Ejercicios/src/Main.java
+++ 15-11Ejercicios/src/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
 
@@ -15,26 +18,160 @@ public class Main {
 //    Bucles anidados para sumar matrices
 
 
-        int[][] matrizA = {{1, 2}, {3, 4}};
-        int[][] matrizB = {{1, 2}, {3, 4}};
-        int[][] resultado = new int[2][2]; // Corrección en la inicialización
+//        int[][] matrizA = {{1, 2}, {3, 4}};
+//        int[][] matrizB = {{1, 2}, {3, 4}};
+//        int[][] resultado = new int[2][2]; // Corrección en la inicialización
 
-        // Multiplicación de matrices
-        for (int i = 0; i < 2; i++) {
-          for (int j = 0; j < 2; j++) {
-            resultado[i][j] = 0; // Inicializa el valor de resultado[i][j]
-            for (int k = 0; k < 2; k++) {
-              resultado[i][j] += matrizA[i][k] * matrizB[k][j];
-            }
-          }
-        }
+    // Multiplicación de matrices
+//        for (int i = 0; i < 2; i++) {
+//          for (int j = 0; j < 2; j++) {
+//            resultado[i][j] = 0; // Inicializa el valor de resultado[i][j]
+//            for (int k = 0; k < 2; k++) {
+//              resultado[i][j] += matrizA[i][k] * matrizB[k][j];
+//            }
+//          }
+//        }
+//
+//        // Imprimir la matriz resultado
+//        for (int i = 0; i < 2; i++) {
+//          for (int j = 0; j < 2; j++) {
+//            System.out.print(resultado[i][j] + " ");
+//          }
+//          System.out.println();
+//        }
+//    Ejercicio3
+//    Escribe un programa que realice una rotación a la derecha en un array de enteros. Es decir,
+//    el último elemento del array debe pasar a ser el primero, y        los demás se desplazan hacia la derecha.
 
-        // Imprimir la matriz resultado
-        for (int i = 0; i < 2; i++) {
-          for (int j = 0; j < 2; j++) {
-            System.out.print(resultado[i][j] + " ");
-          }
-          System.out.println();
-        }
+//    int [] num = {1, 2, 3, 4, 5, 6};
+//    int last = num[num.length - 1];
+//
+//    for (int i = num.length - 1; i > 0; i--) {
+//      num[i] = num[i - 1];
+//    }
+//    num[0] = last;
+//
+//    System.out.println();
+//    for (int i = 0; i < num.length; i++) {
+//      System.out.println(num[i] + " ");
+//
+//    }
+
+//    Ejercicio 4: Determinar si un número es positivo, negativo o cero
+//    Escribe un programa que haga lo siguiente:
+//
+//    Pida al usuario que ingrese un número.
+//            Determine si el número ingresado es positivo, negativo o cero.
+//    Muestra un mensaje correspondiente.
+//    Pista:
+//
+//    Usa la estructura if-else para realizar las comprobaciones.
+//    Recuerda utilizar la clase Scanner para recibir el input del usuario.
+
+//    Scanner scan = new Scanner(System.in);
+//
+//    System.out.println("ingrese un numero");
+//    int num = scan.nextInt();
+//
+//
+//    if (num > 0) {
+//      System.out.println("positivo");
+//    }
+//      else if (num < 0) {
+//      System.out.println("negativo");
+//    }
+//      else {
+//      System.out.println("es igual a cero");
+//    }
+//scan.close();
+
+//    Ejercicio4: Contar palabras en una cadena
+//
+//    Escribe un programa que reciba una cadena de texto del usuario y cuente cuántas palabras contiene.
+//    Una palabra es una secuencia de caracteres delimitada por espacios.
+
+
+//      Scanner scan = new Scanner(System.in);
+//
+//    System.out.println("ingrese una cadena de texto");
+//    String texto = scan.nextLine();
+//    String[] palabras = texto.split(" ");
+//
+//    int cantidadPalabras = palabras.length;
+//
+//    System.out.println(cantidadPalabras);
+
+
+//    Ejercicio 5: Par o impar
+//    Escribe un programa que pida al usuario un número y determine si es par o impar usando un if-else.
+//    Scanner scan = new Scanner(System.in);
+//
+//    System.out.println("ingrese un numero");
+//    int num = scan.nextInt();
+//
+//    if (num % 2 == 0 ) {
+//      System.out.println("el numero es par");
+//    }
+//    else {
+//      System.out.println("el numero es impar");
+//    }
+
+//    Ejercicio 2: Sumar los números de un arreglo
+//    Crea un programa que sume todos los números de un array. Usa un bucle for para recorrer el array y calcular la suma.
+
+//    int [] num = {1, 2, 3, 4 , 5, 6 ,87 ,65, 35};
+//    int suma = 0;
+//    for (int i = 0; i < num.length; i++) {
+//      suma = suma + num[i];
+//      System.out.println(suma);
+//    }
+//    Ejercicio 3: Contar vocales en una cadena
+//    Escribe un programa que reciba una cadena de texto ingresada por el usuario y luego cuente cuántas vocales (a, e, i, o, u) contiene.
+
+    Scanner scan = new Scanner(System.in);
+
+/*    System.out.println(" Escribe una cadena de texto ");
+    String texto = scan.nextLine();
+
+    int contarvocales = 0;
+    int contarConsonantes = 0;
+
+    for (int i = 0; i < texto.length(); i++) {
+    char letra = texto.charAt(i);
+      if ("aeiouAEIOU".indexOf(letra) != -1){
+        contarvocales++;
+      }else if (Character.isLetter(letra)){
+        contarConsonantes++;
       }
     }
+      System.out.println("Hay: " + contarvocales + " vocales");
+    System.out.println("Hay: " + contarConsonantes + " consonante");*/
+
+
+//    System.out.println("ingrese una palabra");
+//    String palabra = scan.nextLine().toLowerCase();
+//
+//    String invertida = "";
+//    for (int i = palabra.length() -1 ; i>= 0;  i--) {
+//      invertida += palabra.charAt(i);
+//    }
+//    if (palabra.equals(invertida)) {
+//      System.out.println("la palabra es un palindromo");
+//    }else {
+//      System.out.println("la palabra no es un palindromo");
+//    }
+
+  /*  System.out.println("ingrese una palabra");
+    String palabra = scan.nextLine().toLowerCase();
+    int contarConsonante = 0;
+
+    for (int i = 0; i < palabra.length(); i++) {
+    char letra = palabra.charAt(i);
+      if (Character.isLetter(letra) && "aeiou".indexOf(letra) == -1) {
+        contarConsonante++;
+      }
+    }
+    System.out.println("hay: " + contarConsonante + "consonantes");*/
+  }
+
+}
