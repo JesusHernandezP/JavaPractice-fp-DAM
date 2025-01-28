d0c0a5492a7b29d9e5d379f2714d36c1a33317b9
Jesus Hernandez <jahp88x@gmail.com>
2025-01-28 12:39:17 +0100

    Practicas de ejercicios con clases, atributos y constructores

--- Ejercicios01-27-25/src/Main.java
+++ Ejercicios01-27-25/src/Main.java
@@ -4,16 +4,57 @@ public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Ingrese un numero");
-    int numeroIngresado = scanner.nextInt();
 
-    if (numeroIngresado % 2 == 0) {
-      System.out.println("El numero es par");
+//Ejercicio1
+    int num1 = 3;
+    int num2 = 5;
+    int suma = num1 + num2;
+
+    System.out.println(suma);
+//------------------------------- ----------------
+
+//Ejercicio2
+    System.out.println("Ingrese un numero entero");
+    int numeroEntero = scanner.nextInt();
+
+    if (numeroEntero % 2 == 0) {
+      System.out.println("El numero ingresado es par");
     }
     else {
-      System.out.println("El numero es impar");
+      System.out.println("El numero ingresado es impar");
     }
-      scanner.close();
-      }
+    scanner.close();
+//------------------------------- ----------------
+
+
+//  Ejercicio3
+
+
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(i);
+    }
+
+//------------------------------- ----------------
+
+//Ejercicio4
+
+    int [] arrayNum = {5 , 6, 9, 8, 4};
+
+    for (int i = 0; i < arrayNum.length; i++) {
+
+      System.out.println(arrayNum[i]);
+    }
+//------------------------------- ----------------
+
+//  Ejercicio5
+
+
+
+persona("Jesus" , "34" );
+
+}
+     static void persona (String nombre, String edad){
+       System.out.println("hola mi nombre es: " + nombre + " y tengo : " + edad);
+    }
 
 }
